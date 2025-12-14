b61da7330aad05870b95307e2180bf7a3f046550
Anthonia Okenwa <divinemercy303@gmail.com>
2025-12-14 13:19:43 +0300

    Fix helm provider by separating kubernetes provider (CI-safe)

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,4 +1,22 @@
 ############################################
+# Kubernetes Provider
+############################################
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(
+    aws_eks_cluster.eks.certificate_authority[0].data
+  )
+  token = data.aws_eks_cluster_auth.eks.token
+}
+
+############################################
+# Helm Provider (uses kubernetes provider)
+############################################
+
+provider "helm" {}
+
+############################################
 # EKS Authentication
 ############################################
 
